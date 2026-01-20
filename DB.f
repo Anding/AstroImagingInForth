ae9ef01799d8ba82bc9c1dfd260561ddd5b9fd6b
Andrew Read <andrew81244@outlook.com>
2026-01-20 10:28:59 +0800

    restructured user lexicon definitions

--- DB.f
+++ DB.f
@@ -5,19 +5,15 @@ include "%idir%\User001.f"
 
 \ define local values for this rig at this observatory
 \ 2280	value focuser.default.position		( typical focus position, Takahashi focuser)
-5200    value focuser.default.position		( typical focus position, 2047 focuser)
-6000    value focuser.default.maxsteps		\ just within full range of travel, to protect the telescope
-80      value focuser.default.backlash		\ as measured by experiment on this rig
-0	    value focuser.default.reverse		\ focuser reverse depends on mounting direction
-100		value camera.default.gain
-0	    value camera.default.offset
-light frames
-
-\ populate expected hardware values (mainly for FITS key information)
+5200    -> focuser.default.position		( typical focus position, 2047 focuser)
+6000    -> focuser.default.maxsteps		\ just within full range of travel, to protect the telescope
+80      -> focuser.default.backlash		\ as measured by experiment on this rig
+0       -> focuser.default.reverse		\ focuser reverse depends on mounting direction
+100     -> camera.default.gain
+0       -> camera.default.offset
 192 168 0 15 toIPv4 -> 10Micron.IP						
-20 secs -> exposure.duration
 s" 160.0"		 	$-> rig.aperature_dia
-s" 17000.0" 	$-> rig.aperature_area
+s" 17000.0"         $-> rig.aperature_area
 s" 530.0"			$-> rig.focal_len
 s" 3.3"				$-> rig.focal_ratio
 \ s" 4.2" 			$-> focuser.stepsize ( um/step, Takahashi focuser)
@@ -25,27 +21,9 @@ s" 2.8"             $-> focuser.stepsize ( um/step, 2047 focuser)
 s" Takahashi Epsilon 160ED " $-> rig.telescope
 s" github.com/Anding/AstroImagingInForth" $-> rig.software
 s" Anding" $-> obs.observer
+light frames
+20 secs duration
 	
-: initialize
-	\ prepare the focuser
-	focuser.default.reverse  ->focuser_reverse
-	focuser.default.backlash ->focuser_backlash
-	focuser.default.maxsteps ->focuser_maxsteps
-	focuser.default.position ->focuser_position	
-	camera.default.gain ->camera_gain
-	camera.default.offset ->camera_offset
-	\ allocate the ForthXISF image structure depending on the camera sensor size
-	camera_pixels 1 ( width height bitplanes) allocate-image ( img) -> image		
-;
-
-: finalize
-		wait-wheel
-		0 ->wheel_position
-		wait-focuser
-		focuser.default.position ->focuser_position	
-		image free-image
-;
-
 \ filter wheel settings
 BEGIN-ENUM
 	+ENUM LUM
@@ -57,7 +35,7 @@ BEGIN-ENUM
 	+ENUM OIII
 END-ENUM
 
-BEGIN-ENUMS DB_FITSfilterBand
+BEGIN-ENUMS DB_filterBand
 	+" LUM"
 	+" RED"
 	+" GREEN"
@@ -67,9 +45,9 @@ BEGIN-ENUMS DB_FITSfilterBand
 	+" OIII"
 END-ENUMS
 
-ASSIGN DB_FITSfilterBand TO-DO FITSfilterBand
+ASSIGN DB_filterBand TO-DO filterBand
 	
-BEGIN-ENUMS DB_FITSfilterSpec
+BEGIN-ENUMS DB_filterSpec
 	+" Astronomik UV-IR-BLOCK L-2"
 	+" Astronomik Deep-Sky RGB"
 	+" Astronomik Deep-Sky RGB"
@@ -79,5 +57,5 @@ BEGIN-ENUMS DB_FITSfilterSpec
 	+" Astronomik MaxFR 6nm"
 END-ENUMS
 
-ASSIGN DB_FITSfilterSpec TO-DO FITSfilterSpec
+ASSIGN DB_filterSpec TO-DO filterSpec
 	
--- User001.f
+++ User001.f
@@ -1,36 +1,44 @@
-\ integrate the hardware (camera, focuser, filter-wheel and the mount) to create a user lexionary for interactive astroimaging
-
+\ integrate the hardware (camera, focuser, filter-wheel and the mount) and extend the user lectionary for integrated hardware
 NEED Forth-map
 NEED ForthASI
 NEED ForthEFW
 NEED ForthEAF
 NEED Forth10Micron
 NEED ForthXISF
+NEED ForthVT100
 
-0 value image							\ pointer to a ForthXISF image structure
-20 Secs value exposure.duration	\ exposure duration in micro seconds
+0 value image                           \ pointer to a ForthXISF image structure
+0 value focuser.default.position        \ typical focus position
+0 value focuser.default.maxsteps		\ just within full range of travel, to protect the telescope
+0 value focuser.default.backlash        \ as measured by experiment on this rig
+0 value focuser.default.reverse	        \ focuser reverse depends on mounting direction
+0 value camera.default.gain
+0 value camera.default.offset
 
 : connect ( --)
-\ connect all hardware	
-	\ activate the camera
+\ connect all hardware and allocate necessary resources
+
 	scan-cameras 
 	0 add-camera
 	0 use-camera
+	camera.default.gain   ->camera_gain
+	camera.default.offset ->camera_offset
+	camera_pixels 1 ( width height bitplanes) allocate-image ( img) -> image	
 
-	
-	\ activate the filter wheel
 	scan-wheels
 	0 add-wheel
 	0 use-wheel
+	0 ->wheel_position
 	
-	\ activate the focuser
 	scan-focusers
 	0 add-focuser
 	0 use-focuser
+	focuser.default.reverse  ->focuser_reverse
+	focuser.default.backlash ->focuser_backlash
+	focuser.default.maxsteps ->focuser_maxsteps
+	focuser.default.position ->focuser_position	
 	
-	\ connect to the mount
 	add-mount	
-	
 ;
 
 : check ( --)
@@ -42,17 +50,21 @@ NEED ForthXISF
 ;
 
 : disconnect ( --)
-\ disconnect all hardware
-
-	0 remove-wheel	
+\ disconnect all hardware and free accociated resources
+    0 ->wheel_position wait-wheel
+	0 remove-wheel
+	
+	focuser.default.position ->focuser_position wait-focuser
 	0 remove-focuser
+	
 	0 remove-camera	
+    image free-image
+    
 	remove-mount
 ;
 
-: expose ( --)
-\ take an image
-	exposure.duration ->camera_exposure
+: (expose) ( -- IOR)
+\ internal word of expose
 	start-exposure
  	\ add FITS keys
  	image FITS_MAP @ add-cameraFITS 	 		\ must begin the FITS header with add-cameraFITS	
@@ -62,96 +74,27 @@ NEED ForthXISF
  	image FITS_MAP @ add-focuserFITS	
  	image FITS_MAP @ add-mountFITS
  	\ wait for the exposure to complete
-	camera_exposure 1000 / 100 + ms	
-	image IMAGE_BITMAP image image_size ( addr n) download-image
-	image save-XISFimage					
-	CR image XISF_FILEPATH_BUFFER buffer-to-string type
+	camera_exposure 1000000 / ( secs)
+	dup 1 >= if 
+	    .countdown ( flag) ?dup if 
+	        cr ." exposure stopped" .>E cr exit 
+	    then
+	else
+	    camera_exposure 1000 / ms 
+	then
+	image IMAGE_BITMAP image image_size ( addr n) download-image 0 
+;
+
+: expose ( --)
+\ take an image and save it in XISF format
+    (expose) if exit then 
+    image save-XISFimage					
+    cr image XISF_FILEPATH_BUFFER buffer-to-string .> cr
 ;
 
 : exposeFITS ( --)
-\ take an image
-	exposure.duration ->camera_exposure
-	start-exposure
- 	\ add FITS keys
- 	image FITS_MAP @ add-cameraFITS 	 		\ must begin the FITS header with add-cameraFITS	
- 	image FITS_MAP @ add-observationFITS
- 	image FITS_MAP @ add-rigFITS	
- 	image FITS_MAP @ add-wheelFITS
- 	image FITS_MAP @ add-focuserFITS	
- 	image FITS_MAP @ add-mountFITS
-	
- 	\ wait for the exposure to complete
-	camera_exposure 1000 / 100 + ms	
-	image IMAGE_BITMAP image image_size ( addr n) download-image
+\ take an image and save it in FITS format
+    (expose) if exit then 
 	image save-FITSimage					
 	CR image FITS_FILEPATH_BUFFER buffer-to-string type
 ;
-
-: duration ( us --)
-\ set the exposure duration
-	-> exposure.duration
-;
-
-: duration? ( -- us)
-	exposure.duration cr . cr
-;
-
-: frames	( n --) 	\ see ForthXISF\properties_obs.f
-\ set the bias, dark, flat, light frame type
-	-> obs.type
-;
-
-: frames? ( --)
-	obs.type observationType
-;
-
-: focus-to ( pos --)
-	wait-focuser
-	->focuser_position 
-	wait-focuser
-;
-
-: focus-by ( delta --)
-	wait-focuser
-	focuser_position + 
-	->focuser_position
-	wait-focuser
-;
-
-: focus? ( -- pos)
-	focuser_position cr . cr
-;	
-
-: filter ( pos --)
-	->wheel_position
-	wait-wheel
-;
-
-: filter? ( --)
-	wheel_position FITSfilterBand cr type cr
-;
-
-: unpark ( --)
-	10u.unpark
-	10u.StartTracking
-;
-
-: park
-	10u.park
-;
-		
-
-: goto ( RA Dec --)
-\ slew the mount to an equatorial coordinate
-	->mount_equatorial ( RA DEC --)
-;
-
-: goto? ( --)
-\ obtain the mount position in equatorial coordinates
-	mount_equatorial swap CR ~~~. ~~~. CR
-;
-
-: object ( caddr u --)
-	$-> obs.object
-;
-
